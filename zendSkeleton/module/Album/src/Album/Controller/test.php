5fa5b48d9c8bedb849fc78ed95ab71ee457eadb1
miyko <mrod@mizkoa.com>
2013-08-05 16:18:41 +0200

    testing

---
+++ zendSkeleton/module/Album/src/Album/Controller/test.php
@@ -0,0 +1,17 @@
+<?php
+
+/*  delete me
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of test
+ *
+ * @author mario
+ */
+class test {
+    //put your code here
+}
+
+?>
